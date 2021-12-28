41bf0becc24f37eab292f9497f595cfeb687f6de
Huan Suh <huansuh@gmail.com>
2021-12-28 15:04:48 +0900

    ScreenCaptureLayoutBuilder : add parameter(ScreenCaptureController)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return ScreenCaptureView(
-      layoutBuilder: (_, captureView, ctrl) {
+      layoutBuilder: (_, ctrl, captureView) {
         return Scaffold(
           appBar: AppBar(
             title: Text(widget.title),
